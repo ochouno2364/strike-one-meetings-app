f98a6f703b4bade7c9a8c158475ad0c5d06afc06
Ochouno2364 <ochouno2364@gmail.com>
2025-02-19 19:09:53 -0500

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+const express = require('express'); 
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+// MIDDLEWARE 
+
+
+
+// ROUTES 
+
+
+
+// PORT LISTENER
+app.listen(3000, ()=> {
+    console.log('Listening on Port 3000')
+});
